b1f65cc2a18282afc9742a7babc8b15704e0dacd
rieruxcii <101130711+rieruxcii@users.noreply.github.com>
2023-03-30 15:05:50 +0000

    Thu, Mar 30, 2023, 10:05 PM +07:00

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
-int main()
-{
-    std::cout <<
+int main() {
+
+  int guess;
+
+  int tries = 0;
+
+  std::cout << "I have a number 1-10.\n";
+  std::cout << "Please guess it: ";
+  std::cin >> guess;
+
+  // Write a while loop here:
+  
+
+
+
+
+
+  if (guess == 8) {
+
+    std::cout << "You got it!\n";
+
+  }
+
 }
