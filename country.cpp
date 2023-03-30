d70e265ef4d874db9815f78c4cbf79d5b09b9b27
rieruxcii <101130711+rieruxcii@users.noreply.github.com>
2023-03-30 15:38:20 +0000

    Thu, Mar 30, 2023, 10:38 PM +07:00

---
+++ country.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string country;
+    
+    cout << "Where do you live?" << endl;
+    cin >> country;
+    cout << "I've heard great things about " << country << " I'd like to go there sometime." << endl;
+
+    return 0;
+}
